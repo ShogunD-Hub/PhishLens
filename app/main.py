f19b52c1432704fe3bcf1859f454fef11015262f
ShogunD-Hub <arhu.james@gmail.com>
2026-08-18 13:16:57 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from app.api import analyze
+
 
 app = FastAPI(
     title="PhishLens",
     description="Defensive phishing email analysis platform",
     version="0.1.0"
 )
+
+
+app.include_router(analyze.router)
 
 
 @app.get("/")
